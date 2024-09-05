b63adf042f6045cf3cf2b1cf91fcc21ad23e7cd7
Carlos Eduardosp <carlos.spadilha@yahoo.com.br>
2024-09-05 06:36:51 -0300

    feat: atualizando versão link conexão com banco

--- src/infra/connection.py
+++ src/infra/connection.py
@@ -7,7 +7,8 @@ class ConnectionDB:
 
         # URL de conexão fornecida pelo MongoDB Atlas
         # Substitua "<your-cluster-url>" pelo seu URL real. Normalmente, ele se parece com 'cluster0.mongodb.net'.
-        self.__url_conexao = "mongodb+srv://guitarristas2004:EzlSVlyCrTxMfEKe@cluster0.wi3ae.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+        #self.__url_conexao = "mongodb+srv://guitarristas2004:EzlSVlyCrTxMfEKe@cluster0.wi3ae.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+        self.__url_conexao = "mongodb+srv://guitarristas2004:EzlSVlyCrTxMfEKe@cluster0.wi3ae.mongodb.net/?retryWrites=true&w=majority&tls=true"
 
         # Criando uma instância de MongoClient e conectando-se ao cluster
         self.__client = MongoClient(self.__url_conexao)
